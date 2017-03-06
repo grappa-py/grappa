0a489a902d41d6b952e663ba617903f445c177cb
Tomas Aparicio <tomas@aparicio.me>
2017-03-06 00:12:54 +0000

    fix(log): do not test handler type

--- tests/log_test.py
+++ tests/log_test.py
@@ -5,12 +5,9 @@ from grappa import log
 def test_log_module(should):
     log | should.be.a('module')
 
+    log | should.have.property('handler')
     log | should.have.property('log') > should.be.instance.of(logging.Logger)
 
     (log
         | should.have.property('formatter')
         > should.be.instance.of(logging.Formatter))
-
-    (log
-        | should.have.property('handler')
-        > should.be.instance.of(logging.Handler))
