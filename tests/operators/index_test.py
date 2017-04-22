59d4afce6f0936e26d950ea4107a57e94cbeebec
Tomás Aparicio <tomas@aparicio.me>
2017-04-23 00:40:22 +0100

    refactor(index_test): remove commented code

--- tests/operators/index_test.py
+++ tests/operators/index_test.py
@@ -8,24 +8,4 @@ def test_index(should):
 
     [1] | should.have.length.of(1).to.have.index.at(0)
 
-    # ({'foo': {'bar': True}}
-    #     | should.have.key('foo')
-    #     > should.be.a('dict')
-    #     > should.have.key('bar')
-    #     > should.be.equal.to({'bar': True}))
-    #
-    # should({'foo': 'bar'}).have.key('foo') > should.be.equal.to('bar')
-    # should({'foo': 'bar'}).have.key('foo').which.should.be.equal.to('bar')
-    #
-    # with pytest.raises(AssertionError):
-    #     {'foo': 'bar'} | should.have.key('bar')
-    #
-    # with pytest.raises(AssertionError):
-    #     [] | should.have.key('bar')
-    #
-    # with pytest.raises(AssertionError):
-    #     should({'foo': 'bar'}).have.key('foo') > should.be.equal.to('foo')
-    #
-    # with pytest.raises(AssertionError):
-    #     (should({'foo': 'bar'}).have.key('foo')
-    #         .which.should.be.equal.to('pepe'))
+  
